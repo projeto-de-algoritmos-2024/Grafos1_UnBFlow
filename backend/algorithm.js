c70bf6a52e4e45c80fe446937af4cc16dadc987d
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-11-11 14:07:07 -0300

    feat(backend): add equivalence between classes :)

--- backend/algorithm.js
+++ backend/algorithm.js
@@ -3,7 +3,7 @@ const PriorityQueue = require("./priority_queue.js")
 const data = require("./disciplinas.json")
 //import data from "./disciplinas.json"
 
-const goal = "FGA0003"
+const goal = "FGA0071"
 
 /**
  * Description
@@ -48,9 +48,25 @@ for (const department of data)
 
 			/** @type Set<string> */ (equivalent.get(equiv.code)).add(course.code)
 		}
-	}
 
-console.log(equivalent.get("FGA0146"))
+		// Link co-dependency course to main one if it's not mapped yet.
+		for (const co of course.co)
+		{
+			if (!equivalent.has(course.code))
+			{
+				equivalent.set(course.code, new Set([]))
+			}
+
+			/** @type Set<string> */ (equivalent.get(course.code)).add(co.code)
+
+			if (!equivalent.has(co.code))
+			{
+				equivalent.set(co.code, new Set([]))
+			}
+
+			/** @type Set<string> */ (equivalent.get(co.code)).add(course.code)
+		}
+	}
 
 if (!equivalent.has(goal))
 	throw Error(`Class ${goal} is not available!`)
@@ -75,7 +91,22 @@ while (queue.length)
 
 	// Class that is being offered.
 	if (course)
+	{
 		classes.set(pre, course.pre.map(item => item.code).concat(course.equiv.map(item => item.code)))
+
+		// Add equivalent or co-equivalent classes that is being offered!
+		//if (equivalent.has(pre))
+		//{
+		//	const equivs = /** @type Set<string> */ (equivalent.get(pre))
+		//	for (const equiv of equivs)
+		//	{
+		//		/** @type Array<string> */ (classes.get(pre)).push(equiv)
+
+		//		queue.push(equiv)
+		//	}
+		//}
+
+	}
 
 	// Is not being offered.
 	else {
@@ -85,10 +116,11 @@ while (queue.length)
 		if (equivalent.has(pre))
 		{
 			const equivs = /** @type Set<string> */ (equivalent.get(pre))
-			for (const equiv of equivs) 
+			for (const equiv of equivs)
 			{
+				/** @type Array<string> */ (classes.get(pre)).push(equiv)
+
 				queue.push(equiv)
-				classes.get(pre).push(equiv)
 			}
 		}
 
@@ -102,96 +134,98 @@ console.log("classes")
 console.log(classes)
 console.log("\n")
 
-///**
-// * @type {Map<string, number>}}
-// */
-//const out = new Map()
-//
-//for (const [code, pre] of classes)
-//{
-//	if (!out.has(code))
-//		out.set(code, 0)
-//
-//	for (const pre_class of pre)
-//	{
-//		const pre_code = pre_class
-//		if (out.has(pre_code))
-//			out.set(pre_code, /** @type number */ (out.get(pre_code)) + 1)
-//		else
-//			out.set(pre_code, 1)
-//	}
-//}
-//
-//console.log("out")
-//console.log(out)
-//console.log("\n")
-//
-///*
-// * Calculate topological sorting of the graph.
-// */
-//
-//
-///**
-// * @param {{from: string, to: string}} a
-// * @param {{from: string, to: string}} b
-// */
-//function compare (a, b) {
-//	return a.from.localeCompare(b.from) > 0
-//}
-//
-//const pq = new PriorityQueue(compare)
-//
-//for (const [code, value] of out)
-//	if (!value)
-//		pq.push({
-//			from: code,
-//			to: code
-//		})
-//
-////console.log("PriorityQueue")
-////console.log(pq._heap)
-////console.log()
-//
-///**
-// * @type {Set<{from: string, to: string}>}
-// */
-//const rev = new Set([])
-//
-//while (pq.size())
-//{
-//	const item = /** @type {{from: string, to: string}} */ (pq.pop())
-//
-//	let has_item = false
-//	for (const k of rev)
-//		if (k.from === item.from && k.to === item.to)
-//		{
-//			has_item = true
-//			break
-//		}
-//
-//	if (has_item)
-//		continue
-//
-//	rev.add(item)
-//
-//	// Class is not available.
-//	if (!classes.has(item.from))
-//		continue
-//
-//	for (const pre of /** @type Array<string> */ (classes.get(item.from)))
-//	{
-//		--out[pre]
-//		if (!out[pre])
-//			pq.push({
-//				from: pre,
-//				to: item.from,
-//			})
-//	}
-//}
-//
-//// Reverse the set.
-//const ans = []
-//for (const course of rev)
-//	ans.unshift(course)
-//
-//console.log(ans)
+/**
+ * @type {Map<string, number>}}
+ */
+const out = new Map()
+
+for (const [code, pre] of classes)
+{
+	if (!out.has(code))
+		out.set(code, 0)
+
+	for (const pre_class of pre)
+	{
+		const pre_code = pre_class
+		if (out.has(pre_code))
+			out.set(pre_code, /** @type number */ (out.get(pre_code)) + 1)
+		else
+			out.set(pre_code, 1)
+	}
+}
+
+console.log("out")
+console.log(out)
+console.log("\n")
+
+/*
+ * Calculate topological sorting of the graph.
+ */
+
+
+/**
+ * @param {{from: string, to: string}} a
+ * @param {{from: string, to: string}} b
+ */
+function compare (a, b) {
+	return a.from.localeCompare(b.from) > 0
+}
+
+const pq = new PriorityQueue(compare)
+
+for (const [code, value] of out)
+	pq.push({
+		from: code,
+		to: code
+	})
+
+//console.log("PriorityQueue")
+//console.log(pq._heap)
+//console.log()
+
+/**
+ * @type {Set<{from: string, to: string}>}
+ */
+const rev = new Set([])
+
+while (pq.size())
+{
+	const item = /** @type {{from: string, to: string}} */ (pq.pop())
+
+	let has_item = false
+	for (const k of rev)
+		if (k.from === item.from && k.to === item.to)
+		{
+			has_item = true
+			break
+		}
+
+	if (has_item)
+		continue
+
+	rev.add(item)
+
+	// Class is not available.
+	if (!classes.has(item.from))
+		continue
+
+	for (const pre of /** @type Array<string> */ (classes.get(item.from)))
+	{
+		--out[pre]
+		if (!out[pre])
+			pq.push({
+				from: pre,
+				to: item.from,
+			})
+	}
+}
+
+// Reverse the set.
+const ans = []
+for (const course of rev)
+	ans.unshift(course)
+
+console.log({
+	classes: Array.from(out.keys()),
+	edges: ans
+})
